19efeddf6dd82d1ae0adb32ac377400c7555fc6f
Ostap Konstantinov <ostap.konstantinov@yandex.ru>
2017-03-20 17:23:14 +1000

    Добавлена грязная реализация Очереди

---
+++ Классы на C++/Грязные реализации классов/queue_library.h
@@ -0,0 +1,49 @@
+#include <cstdio>
+
+template<typename T>
+class tqueue {
+   struct node {
+      T val;
+      node* next;
+   };
+private:
+   node* a, *b;
+public:
+   tqueue(void):a(NULL), b(NULL){}
+   ~tqueue() {
+      this->clear();
+   }
+public:
+   void push(const T& v) {
+       node* n = new node();
+       n->next = NULL;
+       n->val  = v;
+       if(a != NULL) {
+           b->next = n;
+           b = n;
+       } else
+           a = b = n;
+   }
+
+   void  pop(void){
+       node* t = a;
+       if(t != NULL){
+           a = a->next;
+           delete t;
+       }
+       if(a == NULL)
+          b = NULL;
+   }
+
+   void clear(void){
+       while(!empty())
+          pop();
+   }
+
+   T& front(void) { return a->val; }
+   T& front(void) const { return a->val; }
+
+   bool empty(void) const {
+       return (a == NULL);
+   }
+};
