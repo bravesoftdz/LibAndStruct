0b0e85a8ef85799cd230659fcc7e2c4ac6f8e3dd
Ostap Konstantinov <ostap.konstantinov@yandex.ru>
2017-03-27 22:41:54 +1000

    Исправлено форматирование кода
    В некоторых местах стояли пробелы вместо ТАБов.

--- Классы на C++/Грязные реализации классов/queue_library.h
+++ Классы на C++/Грязные реализации классов/queue_library.h
@@ -2,48 +2,48 @@
 
 template<typename T>
 class tqueue {
-   struct node {
-      T val;
-      node* next;
-   };
+	struct node {
+		T val;
+		node* next;
+	};
 private:
-   node* a, *b;
+	node* a, *b;
 public:
-   tqueue(void):a(NULL), b(NULL){}
-   ~tqueue() {
-      this->clear();
-   }
+	tqueue(void):a(NULL), b(NULL){}
+	~tqueue() {
+		this->clear();
+	}
 public:
-   void push(const T& v) {
-       node* n = new node();
-       n->next = NULL;
-       n->val  = v;
-       if(a != NULL) {
-           b->next = n;
-           b = n;
-       } else
-           a = b = n;
-   }
+	void push(const T& v) {
+		node* n = new node();
+		n->next = NULL;
+		n->val  = v;
+		if(a != NULL) {
+			b->next = n;
+			b = n;
+		} else
+			a = b = n;
+	}
 
-   void  pop(void){
-       node* t = a;
-       if(t != NULL){
-           a = a->next;
-           delete t;
-       }
-       if(a == NULL)
-          b = NULL;
-   }
+	void  pop(void){
+		node* t = a;
+		if(t != NULL){
+			a = a->next;
+			delete t;
+		}
+		if(a == NULL)
+			b = NULL;
+	}
 
-   void clear(void){
-       while(!empty())
-          pop();
-   }
+	void clear(void){
+		while(!empty())
+			pop();
+	}
 
-   T& front(void) { return a->val; }
-   T& front(void) const { return a->val; }
+	T& front(void) { return a->val; }
+	T& front(void) const { return a->val; }
 
-   bool empty(void) const {
-       return (a == NULL);
-   }
+	bool empty(void) const {
+		return (a == NULL);
+	}
 };
